c8326dd76c34fc5b0b49fbb6b0cad6c226ccd50a
Chika Marsha F <cmrshf@gmail.com>
2022-06-08 15:27:31 +0700

    Create Python Fundamental.py

---
+++ Python Fundamental/Python Fundamental.py
@@ -0,0 +1,56 @@
+# Code Hello World
+print("Hello World")
+print(5+1)
+
+    # Tugas Praktek
+    print(10*2+5)
+    print("Academy DQLab")
+
+
+# Comment pada Python
+print("Ini adalah sebuah baris kode") #ini adalahcotoh comment dan tidak akan tercetak
+
+    # Tugas Praktek
+    print(10*2+5) #fungsi matematika
+    print("Academy DQLab") #fungsi mencetak kalimat
+
+
+# Printing Data Type
+## Tipe Data Boolean
+print(True)
+
+## Tipe Data String
+print("Ayo belajar Python")
+print('Belajar Python sangat Mudah di DQLAB')
+
+## Tipe Data Integer
+print(20)
+
+## Tipe Data Float
+print(3.14)
+
+## Tipe Data List
+print([1,2,3,4,5])
+print(["satu", "dua", "tiga"])
+
+## Tipe Data Tuple
+print((1,2,3,4,5))
+print(("satu", "dua", "tiga"))
+
+## Tipe Data Dictionary
+print({"nama":"Budi", 'umur':20})
+
+## Tipe Data Dictionary dimasukan ke dalam variabel biodata
+biodata = {"nama":"Andi", 'umur':21} #proses inisialisasi variabel biodata
+print(biodata) #proses pencetakan variabel biodata yang berisi tipe data Dictionary
+type(biodata) #fungsi untuk mengecek jenis tipe data. akan tampil <class 'dict'>
+
+    #Tugas Praktek
+    var_string = "Belajar Python DQLAB"
+    var_int = 10
+    var_float = 3.14
+    var_list = [1,2,3,4]
+    var_tuple = ("satu", "dua", "tiga")
+    var_dict = {"nama":"Ali", 'umur':20}
+
+    
